92c57ba17998b21d1a75e17e253f840e8f7d8b0d
yakub499 <57057630+yakub499@users.noreply.github.com>
2022-01-25 04:01:15 -0800

    Update lambda_function.py
    test

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,3 @@
-import pandas as pd
+#!/usr/bin/env python3
 
-def lambda_handler(event, context):
-    d = {'col1': [1,2], 'col2': [3,4]}
-    df = pd.DataFrame(data=d)
-    print(df)
-    print('Done x1.1')
+print('Hello World!')
